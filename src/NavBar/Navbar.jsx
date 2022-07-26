88cb8a737ca6800228b22d21575d72fd4e8a07fb
Franc <francissteven1@gmail.com>
2022-07-26 14:02:09 +0300

    add main navbar

--- src/NavBar/Navbar.jsx
+++ src/NavBar/Navbar.jsx
@@ -1,1 +1,138 @@
-export default function Navbar() {}
+export default function Navbar() {
+  return (
+    <nav className="px-2 py-2 bg-white">
+      <div className="container flex flex-wrap justify-between items-center mx-auto">
+        <a href="#" class="flex items-center">
+          {/* image logo  here */}
+          <span className="self-center text-xl font-semibold whitespace-nowrap">
+            Super Six
+          </span>
+        </a>
+        <button
+          data-collapse-toggle="mobile-menu"
+          type="button"
+          className="inline-flex justify-center items-center ml-3 text-gray-800 rounded-lg md:hidden hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-blue-300 dark:text-gray-400 dark:hover:text-white dark:focus:ring-gray-500"
+          aria-controls="mobile-menu-2"
+          aria-expanded="false"
+        >
+          <span className="sr-only">Open main menu</span>
+          <svg
+            className="w-6 h-6"
+            aria-hidden="true"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+              clip-rule="evenodd"
+            ></path>
+          </svg>
+        </button>
+        <div className="hidden w-full md:block md:w-auto" id="mobile-menu">
+          <ul className="flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium">
+            <li>
+              <a
+                href="#"
+                className="block py-2 pr-4 pl-3 text-gray-800 rounded md:p-0 hover:text-gray-900"
+                aria-current="page"
+              >
+                Home
+              </a>
+            </li>
+            <li>
+              <button
+                id="dropdownNavbarLink"
+                data-dropdown-toggle="dropdownNavbar"
+                className="flex justify-between items-center py-2 pr-4 pl-3 w-full font-medium text-gray-800 border-b hover:text-gray-900 md:hover:bg-transparent md:border-0  md:p-0 md:w-auto"
+              >
+                Dropdown{" "}
+                <svg
+                  className="ml-1 w-4 h-4"
+                  aria-hidden="true"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                    clip-rule="evenodd"
+                  ></path>
+                </svg>
+              </button>
+              {/* Dropdown menu  */}
+              <div
+                id="dropdownNavbar"
+                class="hidden z-10 w-44 font-normal bg-white rounded divide-y divide-gray-100 shadow"
+              >
+                <ul
+                  class="py-1 text-sm text-gray-700"
+                  aria-labelledby="dropdownLargeButton"
+                >
+                  <li>
+                    <a
+                      href="#"
+                      className="block py-2 px-4 text-gray-900 hover:text-gray-700"
+                    >
+                      Francis Kikulwe
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href="#"
+                      className="block py-2 px-4 text-gray-900 hover:text-gray-700"
+                    >
+                      Pascal Gihozo
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href="#"
+                      className="block py-2 px-4 text-gray-900 hover:text-gray-700"
+                    >
+                      Samantha Kelosi
+                    </a>
+                  </li>
+                </ul>
+                <div class="py-1">
+                  <a
+                    href="#"
+                    class="block py-2 px-4 text-sm text-gray-800 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-400 dark:hover:text-white"
+                  >
+                    Sign out
+                  </a>
+                </div>
+              </div>
+            </li>
+            <li>
+              <a
+                href="#"
+                className="block py-2 pr-4 pl-3 text-gray-800 border-b hover:text-gray-900 md:hover:bg-transparent md:border-0 md:p-0"
+              >
+                Services
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                className="block py-2 pr-4 pl-3 text-gray-800 border-b hover:text-gray-900 md:hover:bg-transparent md:border-0 md:p-0"
+              >
+                Pricing
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                className="block py-2 pr-4 pl-3 text-gray-800 border-b hover:text-gray-900 md:hover:bg-transparent md:border-0 md:p-0"
+              >
+                Contact
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+}
